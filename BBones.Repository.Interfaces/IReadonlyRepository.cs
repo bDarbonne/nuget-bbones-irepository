9ab024e1c366d9124e339efcf3cde9e988d54faa
Benjamin Darbonne <dev.darbonne.ben@gmail.com>
2022-08-15 17:41:56 -0500

    Migrate readonly repo to async

--- BBones.Repository.Interfaces/IReadonlyRepository.cs
+++ BBones.Repository.Interfaces/IReadonlyRepository.cs
@@ -1,10 +1,11 @@
+using System.Linq.Expressions;
 
 namespace BBones.Repository.Interfaces
 {
     public interface IReadonlyRepository<T> where T : class
     {
-        List<T> Get(Func<T, bool>? predicate = null);
-        T? GetById(object id);
-        T? FirstOrDefault(Func<T, bool> predicate);
+        Task<List<T>> Get(Func<T, bool>? predicate = null);
+        Task<T?> FirstOrDefault(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default);
+        ValueTask<T?> GetById(object id);
     }
 }
